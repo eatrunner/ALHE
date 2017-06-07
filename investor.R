2d28cab6c29219b1b4b2b77fea272dce602b4e05
pitrosk <pitrosk@gmail.com>
2017-06-07 21:28:36 +0200

    Dodane wczytywanie od razu w R plus tworzenie kolumny czasu. Dodatkowo pozmieniane ploty i price nazwane 1-4

--- investor.R
+++ investor.R
@@ -1,3 +1,5 @@
+library(readr)
+library(dplyr)
 
 evaluate <- function(K, invest, maxEURUSD, maxEURPLN, maxUSDPLN, priceEURUSD, priceEURPLN, priceUSDPLN)
 {
@@ -15,25 +17,46 @@ evaluate <- function(K, invest, maxEURUSD, maxEURPLN, maxUSDPLN, priceEURUSD, pr
 N <- 100
 K <- 100
 
-EURPLN <- read.csv("EURPLN2016.csv")
-EURUSD <- read.csv("EURUSD2016.csv")
-USDPLN <- read.csv("USDPLN2016.csv")
+EURPLN <- read_csv("DAT_MT_EURPLN_M1_2016.csv", 
+                   col_names = FALSE, col_types = cols(X1 = col_date(format = "%Y.%m.%d"), 
+                                                       X2 = col_time(format = "%H:%M"), 
+                                                       X7 = col_skip()))
+colnames(EURPLN) <- c("date", "time", "price1", "price2", "price3","price4")
+EURPLN <- EURPLN %>% mutate( time = as.POSIXct(paste(date,time,sep = " ")))
+EURPLN$date <- NULL
 
-plot(EURUSD$price,type = 'l', col = 'black', xlab = "Time", ylab = "EXCH")
+EURUSD <- read_csv("DAT_MT_EURUSD_M1_2016.csv", 
+                   col_names = FALSE, col_types = cols(X1 = col_date(format = "%Y.%m.%d"), 
+                                                       X2 = col_time(format = "%H:%M"), 
+                                                       X7 = col_skip()))
+colnames(EURUSD) <- c("date", "time", "price1", "price2", "price3","price4")
+EURUSD <- EURUSD %>% mutate( time = as.POSIXct(paste(date,time,sep = " ")))
+EURUSD$date <- NULL
+
+USDPLN <- read_csv("DAT_MT_USDPLN_M1_2016.csv", 
+                   col_names = FALSE, col_types = cols(X1 = col_date(format = "%Y.%m.%d"), 
+                                                       X2 = col_time(format = "%H:%M"), 
+                                                       X7 = col_skip()))
+colnames(USDPLN) <- c("date", "time", "price1", "price2", "price3","price4")
+USDPLN <- USDPLN %>% mutate( time = as.POSIXct(paste(date,time,sep = " ")))
+USDPLN$date <- NULL
+
+
+plot(EURUSD$time, EURUSD$price1,type = 'l', col = 'black', xlab = "Time", ylab = "EXCH")
 par(new=TRUE)
-plot(EURPLN$price, type = 'l', col = 'blue', xlab = "Time", ylab = "EXCH")
+plot(EURPLN$time, EURPLN$price1, type = 'l', col = 'blue', xlab = "Time", ylab = "EXCH")
 par(new=TRUE)
-plot(USDPLN$price, type = 'l', col = 'red', xlab = "Time", ylab = "EXCH")
+plot(USDPLN$time, USDPLN$price1, type = 'l', col = 'red', xlab = "Time", ylab = "EXCH")
 
 print(length(data$price))
-maxEURUSD = which(diff(sign(diff(EURUSD$price)))==-2)+1
-maxEURPLN = which(diff(sign(diff(EURPLN$price)))==-2)+1
-maxUSDPLN = which(diff(sign(diff(USDPLN$price)))==-2)+1
-plot(maxEURUSD, EURUSD$price[maxEURUSD], type = 'l', col = 'black', xlab = "Maxima", ylab = "EXCH")
+maxEURUSD = which(diff(sign(diff(EURUSD$price1)))==-2)+1
+maxEURPLN = which(diff(sign(diff(EURPLN$price1)))==-2)+1
+maxUSDPLN = which(diff(sign(diff(USDPLN$price1)))==-2)+1
+plot(EURUSD$time[maxEURUSD], EURUSD$price1[maxEURUSD], type = 'l', col = 'black', xlab = "Maxima", ylab = "EXCH")
 par(new=TRUE)
-plot(maxEURPLN, EURPLN$price[maxEURPLN], type = 'l', col = 'blue', xlab = "Maxima", ylab = "EXCH")
+plot(EURPLN$time[maxEURPLN], EURPLN$price1[maxEURPLN], type = 'l', col = 'blue', xlab = "Maxima", ylab = "EXCH")
 par(new=TRUE)
-plot(maxUSDPLN, USDPLN$price[maxUSDPLN], type = 'l', col = 'red', xlab = "Maxima", ylab = "EXCH")
+plot(USDPLN$time[maxUSDPLN], USDPLN$price1[maxUSDPLN], type = 'l', col = 'red', xlab = "Maxima", ylab = "EXCH")
 
 
 # Niewięcej transakcji niz maximów
@@ -57,4 +80,4 @@ invest <- data.frame(
   EX = rep(1, N)
   )
 
-print(evaluate(K, invest, maxEURUSD, maxEURPLN, maxUSDPLN, EURUSD$price, EURPLN$price, USDPLN$price))
+print(evaluate(K, invest, maxEURUSD, maxEURPLN, maxUSDPLN, EURUSD$price1, EURPLN$price1, USDPLN$price1))
